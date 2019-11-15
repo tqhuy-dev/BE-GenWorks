5dabf30091e263b3f3c67bdfd7a91f87b879f147
Huy <tqhuy1996.developer@gmail.com>
2019-11-15 09:54:17 +0700

    test case sign up body

--- test/users-test.js
+++ test/users-test.js
@@ -30,14 +30,38 @@ describe("Server!", () => {
   //     });
   // });
 
-  it("Test get customer information" ,done => {
+  it("Test get customer information", done => {
     chai
       .request(app)
       .get("/api/v1/users/tqhuy1996.developer@gmail.com")
-      .end((err , res) => {
+      .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.code).to.equal(200)
         done();
+      });
+  });
+
+  it("Test validate email", done => {
+    chai
+      .request(app)
+      .post("/api/v1/users")
+      .send({
+        emaild: "test1.developer@gmail.com",
+        password: "Abc123",
+        first_name: "Tran",
+        last_name: "Quoc Huy",
+        age: 23,
+        birthdate: "842757674",
+        address: {
+          city: "Ho Chi Minh",
+          country: "VietName",
+          address: "184 Dang Van Ngu Street , Phu Nhuan District"
+        }
+      })
+      .end((err , res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.code).to.equal(400);
+        done()
       });
   });
 });
